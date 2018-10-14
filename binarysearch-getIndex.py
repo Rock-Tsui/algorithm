c6495947944c337b423fd8dc33ac1b11310f7cf7
Rock-Tsui <Rock-Tsui@users.noreply.github.com>
2018-10-14 21:41:36 +0800

    update

--- binarysearch-getIndex.py
+++ binarysearch-getIndex.py
@@ -5,19 +5,35 @@ L = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 def get_index(data, sIndex, eIndex):
+    '''递归'''
     global L
-    mid_index = (eIndex - sIndex) // 2 + sIndex
-    
-    if L[sIndex] > data or L[eIndex] < data:
-        return -1
-    if L[mid_index] > data:
-        return get_index(data, sIndex, mid_index)
-    elif L[mid_index] < data:
-        return get_index(data, mid_index + 1, eIndex)
-    else:
-        return mid_index
+    midIndex = (eIndex + sIndex) // 2
 
+    if L[sIndex] > data or L[eIndex] < data:
+         return -1
+    if L[midIndex] > data:
+        return get_index(data, sIndex, midIndex)
+    elif L[midIndex] < data:
+        return get_index(data, midIndex + 1, eIndex)
+    else:
+        return midIndex
+
+def get_index1(alist, data):
+    '''非递归'''
+    n = len(alist)
+    first = 0
+    last = n - 1
+
+    while first <= last:
+        mid = (last + first) // 2
+        if alist[mid] > data:
+            last = mid - 1
+        elif alist[mid] <data:
+            first = mid + 1
+        else:
+            return mid
+    return -1
 
 if __name__ == '__main__':
-    index = get_index(9, 0, 8)
-    print(index)
+    #print(get_index(9, 0, 8))
+    print(get_index1(L, 1))
--- binarysearch.py
+++ binarysearch.py
@@ -1,12 +1,13 @@
 #coding=utf-8
 '''在一个已经排序的Int数组中，查找某个number，
-如果存在这个number，返回在数组的位置，反之返回-1'''
+如果存在这个number，返回True，反之返回False'''
 
 
 def binary_chop(alist, data):
     '''递归解决二分查找'''
     n = len(alist)
     mid = n//2
+
     if n < 1:
         return False
     if alist[mid] > data:
@@ -16,6 +17,24 @@ def binary_chop(alist, data):
     else:
         return True
 
+def binary_chop1(alist, data):
+    '''非递归解决二分查找'''
+    n = len(alist)
+    first = 0
+    last = n - 1
+
+    while first <= last:
+        mid = (last + first) // 2
+        if alist[mid] > data:
+            last = mid - 1
+        elif alist[mid] <data:
+            first = mid + 1
+        else:
+            return True
+    return False
+
+
+
 if __name__ == '__main__':
     alist = [2,4,5,12,14,23]
-    index = binary_chop(alist, 23)
+    print(binary_chop1(alist, 23))
