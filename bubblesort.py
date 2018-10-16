7c9d20b2030c17d6b2cd5371502462d17d494563
Rock-Tsui <Rock-Tsui@users.noreply.github.com>
2018-10-16 11:03:11 +0800

    update

--- bubblesort.py
+++ bubblesort.py
@@ -1,13 +1,11 @@
-def sorted(L):
+def bubble_sort(L):
     length = len(L)
-    for i in range(length):
+    for i in range(length-1):
         for j in range(i+1,length):
             if L[i] > L[j]:
-                L[i] += L[j]
-                L[j] = L[i] - L[j]
-                L[i] -= L[j]
+                L[i], L[j] = L[j], L[i]
     print(L)
 
 if __name__ == '__main__':
-    L = [34,1,11,8,13,7,20,3,5]
-    sorted(L)
+    L = [34, 1, 11, 8, 13, 7, 20, 3, 5]
+    bubble_sort(L)
